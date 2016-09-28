03adaf354bde2d8163c61655c4af28784f31fa68
Evgeniy Nagalskiy <ltcolumbo@MacBook-Pro-Evgeniy.local>
2016-09-28 10:16:51 +0300

    Tachyons boilerplate gulp/postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,86 @@
 'use strict';
 
 var gulp                   = require('gulp'),
-    sass                   = require('gulp-sass'),
-	  autoprefixer           = require('gulp-autoprefixer'),
-  	browserSync            = require('browser-sync').create(),
-  	watch                  = require('gulp-watch');
+    postcss                = require('gulp-postcss'),
+    autoprefixer           = require('autoprefixer'),
+    cssnext                = require('cssnext'),
+    precss                 = require('precss'),
+    browserSync            = require('browser-sync').create(),
+    watch                  = require('gulp-watch');
 
 var task = {};
 
 var path = {
-    build: {
-    	html: 'dist/',
-      stylesheets: 'dist/assets/stylesheets/',
-      img: 'dist/assets/images/',
-      javascript: 'dist/assets/javascript/',
-      fonts: 'dist/assets/fonts/'
-    },
-    src: {
-    	html: 'src/*.html',
-      stylesheets: 'src/assets/stylesheets/main.scss',
-      img: 'src/assets/images/**/*.*',
-      javascript: 'src/assets/javascript/**/*.js',
-      fonts: 'src/assets/fonts/**/*.*'
-    },
-    watch: {
-      html: 'src/**/*.html',
-      stylesheets: 'src/assets/stylesheets/**/*.scss',
-      img: 'src/assets/images/**/*.*',
-      javascript: 'src/assets/javascript/**/*.js',
-      fonts: 'src/assets/fonts/**/*.*'
-    }
+  build: {
+    html: 'dist/',
+    stylesheets: 'dist/assets/stylesheets/',
+    img: 'dist/assets/images/',
+    fonts: 'dist/assets/fonts/',
+    vendors: 'dist/assets/vendors/'
+  },
+  src: {
+    html: 'src/*.html',
+    stylesheets: 'src/assets/stylesheets/main.css',
+    img: 'src/assets/images/**/*.*',
+    fonts: 'src/assets/fonts/**/*.*',
+    vendors: 'src/assets/vendors/**/*.*'
+  },
+  watch: {
+    html: 'src/**/*.html',
+    stylesheets: 'src/assets/stylesheets/**/*.css',
+    img: 'src/assets/images/**/*.*',
+    fonts: 'src/assets/fonts/**/*.*',
+    vendors: 'src/assets/vendors/**/*.*'
+  }
 };
+
 
 // HTML
 gulp.task('html:build', task.html = function () {
-	return gulp.src(path.src.html)
-	  .pipe(gulp.dest(path.build.html))
+  return gulp.src(path.src.html)
+    .pipe(gulp.dest(path.build.html))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
 
 //Stylesheets
-gulp.task('sass:build', function () {
-  return gulp.src(path.src.stylesheets)
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest(path.build.stylesheets))
-    .pipe(browserSync.reload({
-        stream: true
+gulp.task('stylesheets:build', task.stylesheets = function () {
+  //Обработаем postCSS плагинами наш css
+  var processors = [
+    autoprefixer,
+    cssnext,
+    precss
+  ];
+  gulp.src(path.src.stylesheets)
+    .pipe(postcss(processors))
+    .pipe(gulp.dest(path.build.stylesheets)) //И в build
+    .pipe(browserSync.reload({ //Обновим окно браузера
+      stream: true
     }));
 });
 
+//CSS @import
+// var options = {};
+// gulp.task("import:build", function() {
+//   gulp.src(path.src.stylesheets)
+//     .pipe(cssimport(options))
+//     .pipe(gulp.dest(path.build.stylesheets));
+// });
 
-// Javascript
-gulp.task('javascript:build', task.javascript = function () {
-	return gulp.src(path.src.javascript)
-    .pipe(gulp.dest(path.build.javascript))
+// FONTS
+gulp.task('fonts:build', task.fonts = function () {
+  gulp.src(path.src.fonts)
+    .pipe(gulp.dest(path.build.fonts))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
 
-// FONTS
-gulp.task('fonts:build', task.fonts = function () {
-  return gulp.src(path.src.fonts)
-    .pipe(gulp.dest(path.build.fonts))
+// VENDORS
+gulp.task('vendors:build', task.vendors = function () {
+  gulp.src(path.src.vendors)
+    .pipe(gulp.dest(path.build.vendors))
     .pipe(browserSync.reload({
       stream: true
     }));
@@ -76,7 +88,7 @@ gulp.task('fonts:build', task.fonts = function () {
 
 //Images
 gulp.task('img:build', task.img = function () {
-	gulp.src(path.src.img)
+  gulp.src(path.src.img)
     .pipe(gulp.dest(path.build.img))
     .pipe(browserSync.reload({
       stream: true
@@ -89,43 +101,37 @@ gulp.task('server:build', function() {
   browserSync.init({
     port : 3200,
     server: {
-        baseDir: 'dist'
+      baseDir: 'dist'
     },
     open: true
   });
 });
 
-
-gulp.task(
-  'build', [
-    'html:build',
-    'sass:build',
-    'server:build',
-    'img:build',
-    'javascript:build',
-    'fonts:build'
-  ]
-);
+gulp.task('build', [
+  'stylesheets:build',
+  'html:build',
+  'server:build',
+  'img:build',
+  'vendors:build',
+  'fonts:build'
+]);
 
 gulp.task('watch', function () {
-    watch([path.watch.stylesheets], function (event, cb) {
-        gulp.start('sass:build');
-    });
-    watch([path.watch.html], function (event, cb) {
-        gulp.start('html:build');
-    });
-    watch([path.watch.img], function (event, cb) {
-        gulp.start('img:build');
-    });
-    watch([path.watch.javascript], function (event, cb) {
-        gulp.start('javascript:build');
-    });
-    watch([path.watch.fonts], function (event, cb) {
-        gulp.start('fonts:build');
-    });
+  watch([path.watch.stylesheets], function (event, cb) {
+    gulp.start('stylesheets:build');
+  });
+  watch([path.watch.html], function (event, cb) {
+    gulp.start('html:build');
+  });
+  watch([path.watch.img], function (event, cb) {
+    gulp.start('img:build');
+  });
+  watch([path.watch.vendors], function (event, cb) {
+    gulp.start('vendors:build');
+  });
+  watch([path.watch.fonts], function (event, cb) {
+    gulp.start('fonts:build');
+  });
 });
 
 gulp.task('default', ['build', 'watch']);
-
-
-
