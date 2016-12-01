14d8926b87ffe4c1ad84e0b78519da7c7ac035fc
Taylor Bell <taylorbell@gmail.com>
2016-12-01 16:15:03 -0700

    convert button to class

--- src/react-styleguide/src/App.js
+++ src/react-styleguide/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Button from './components/Buttons/Button'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Button>
+          Click Me Bro
+        </Button>
+        <Button type='warning' />
       </div>
     )
   }
