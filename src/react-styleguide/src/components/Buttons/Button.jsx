18fbdf361343e0cca092e0b4e6f6733ec3c8713f
Taylor Bell <taylorbell@gmail.com>
2016-12-01 16:17:20 -0700

    actually add components directory

---
+++ src/react-styleguide/src/components/Buttons/Button.jsx
@@ -0,0 +1,54 @@
+import React, { Component, PropTypes } from 'react'
+
+export const commonClasses = 'link dib f5 fw6 tracked tc br2 ttu ba'
+export const lgBtnClasses = 'h3 pa3 eh-btn-primary'
+export const ctaBtnClasses = 'lh-solid ph4 pv4 eh-cta'
+export const smBtnClasses = 'h2 lh-solid pa3 eh-btn'
+
+const solidBtnClasses = {
+  success: 'b--transparent bg-green dark-navy hover-bg-dark-green',
+	warning: 'b--transparent bg-yellow dark-navy hover-bg-orange hover-white',
+	danger: 'b--red red hover-dark-navy hover-bg-red', // TODO: Update this
+  primary: 'ph4 pv4 b--transparent bg-blue white hover-bg-dark-blue',
+  default: 'b--white white hover-dark-navy hover-bg-white' // TODO: Update this
+}
+
+const outlineBtnClasses = {
+	success: 'b--green green hover-dark-navy hover-bg-green',
+  warning: 'b--yellow yellow hover-dark-navy hover-bg-yellow',
+	danger: 'b--red red hover-dark-navy hover-bg-red',
+  primary: 'ph4 pv4 b--transparent bg-blue white hover-bg-dark-blue', // TODO: Update this
+  default: 'b--white white hover-dark-navy hover-bg-white'
+}
+
+class Button extends Component { 
+  static propTypes = {
+    href: PropTypes.string,
+    type: PropTypes.string,
+    size: PropTypes.string,
+    outline: PropTypes.bool,
+    children: PropTypes.node
+  }
+
+  static defaultProps = {
+    href: '',
+    type: 'default',
+    size: 'large',
+    outline: false,
+    children: 'Button'
+  }
+
+  render () {
+    const { href, type, size, outline, children } = this.props
+    const btnClasses = outline ? outlineBtnClasses[type] : solidBtnClasses[type]
+    const sizeClasses = size === 'large' ? lgBtnClasses : smBtnClasses
+
+    return (
+      <a href={href} className={`${commonClasses} ${btnClasses} ${sizeClasses}`}>
+        {children}
+      </a>
+    )
+  }
+}
+
+export default Button
