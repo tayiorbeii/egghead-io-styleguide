c7540b88ba93840a84cf3393c6373857f38b226c
Taylor Bell <taylorbell@gmail.com>
2016-12-01 10:11:40 -0700

    get react app in sync with regular styleguide

---
+++ src/react-styleguide/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import 'tachyons'
+import './assets/stylesheets/main.css'
+import App from './App'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
