3f51bd7e5b9e29b2e5bebe85de35a68a3f23010b
Taylor Bell <taylorbell@gmail.com>
2016-12-02 09:55:31 -0700

    buttons working in row but not good on mobile yet

--- src/react-styleguide/src/App.js
+++ src/react-styleguide/src/App.js
@@ -4,12 +4,18 @@ import Button from './components/Buttons/Button'
 class App extends Component {
   render() {
     return (
-      <div>
-        <Button>Default</Button>
-        <Button type='primary' size='cta'>Primary</Button>
-        <Button type='danger' size='small'>Danger</Button>
-        <Button type='warning' outline>Warning</Button>
-        <Button type='success'>Success</Button>
+      <div className='ph3 ph5-ns mt0-ns mb5-ns pt0-ns pb5'>
+        <div className='mw8 center'>
+
+          <h3 className='f2 mb4 mb5-ns mt5 mt6-ns pb2 bb'>Buttons</h3>
+					<div className='flex flex-row-ns justify-around flex-wrap items-center'>
+						<Button>Default</Button>
+						<Button type='primary' size='cta'>Primary</Button>
+						<Button type='success' outline>Success</Button>
+						<Button type='warning' size='small'>Warning</Button>
+						<Button type='danger' size='small' outline>Danger</Button>
+					</div>
+        </div>
       </div>
     )
   }
