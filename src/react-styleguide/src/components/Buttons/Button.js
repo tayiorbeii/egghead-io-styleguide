8351d4b4acb2fa344bbdeb65d2a0c683702c1ede
Taylor Bell <taylorbell@gmail.com>
2016-12-02 10:59:11 -0700

    code font size

--- src/react-styleguide/src/components/Buttons/Button.js
+++ src/react-styleguide/src/components/Buttons/Button.js
@@ -78,27 +78,27 @@ export const ButtonExampleCode = () => {
 	return (
 		<div className='pl4 bl bw4 b--white'>
 			<pre>
-				<code>
+				<code className='f6'>
 					&lt;Button&gt;Default&lt;/Button&gt;
 				</code>
 			</pre>
 			<pre>
-				<code>
+				<code className='f6'>
 					&lt;Button type='primary' size='cta'&gt;Primary&lt;/Button&gt;
 				</code>
 			</pre>
 			<pre>
-				<code>
+				<code className='f6'>
 					&lt;Button type='success' outline&gt;Success&lt;/Button&gt;
 				</code>
 			</pre>
 			<pre>
-				<code>
+				<code className='f6'>
 					&lt;Button type='warning' size='small'&gt;Warning&lt;/Button&gt;
 				</code>
 			</pre>
 			<pre>
-				<code>
+				<code className='f6'>
 					&lt;Button type='danger' size='small' outline&gt;Primary&lt;/Button&gt;
 				</code>
 			</pre>
