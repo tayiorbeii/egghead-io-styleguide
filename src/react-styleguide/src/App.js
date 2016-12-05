854b1136264e204f9fe085b1f8c73c5f9fc06c30
Taylor Bell <taylorbell@gmail.com>
2016-12-05 09:48:10 -0700

    tab to space

--- src/react-styleguide/src/App.js
+++ src/react-styleguide/src/App.js
@@ -5,15 +5,15 @@ import PropTable from './components/GuideElements/PropTable'
 
 class App extends Component {
   render() {
-		const Header = ({title}) => <h3 className='f2 mb4 mb5-ns mt5 mt6-ns pb2 bb'>{title}</h3>
+    const Header = ({title}) => <h3 className='f2 mb4 mb5-ns mt5 mt6-ns pb2 bb'>{title}</h3>
 
     return (
       <div className='ph3 ph5-ns mt0-ns mb5-ns pt0-ns pb5'>
         <div className='mw8 center'>
-					<Header title='Buttons' />
-					<ButtonExamples />
-					<CodeBlock code={ButtonExampleCode} />
-					<PropTable rows={ButtonPropsTable} />
+          <Header title='Buttons' />
+          <ButtonExamples />
+          <CodeBlock code={ButtonExampleCode} />
+          <PropTable rows={ButtonPropsTable} />
         </div>
       </div>
     )
