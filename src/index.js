02e2efc42fbde360c9ebf9453afc2619279641fc
Trevor D. Miller <trevordmiller@icloud.com>
2016-12-13 17:36:03 -0700

    Remove transfer directory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 // TODO: exports the public API for apps that consume egghead style guide components, will get built to the /build folder
+// Could probably just glob all exports from components/ and re-export here
