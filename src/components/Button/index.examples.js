e3e1bc5500cfe30f51fa32b291bdaeaa36c17e0e
Taylor Bell <taylorbell@gmail.com>
2017-01-02 10:45:08 -0700

    start transferring button

--- src/components/Button/index.examples.js
+++ src/components/Button/index.examples.js
@@ -3,8 +3,35 @@ import {storiesOf} from '@kadira/storybook'
 import {stringFixture, clickHandlerFixture} from '../../utils/Fixtures'
 import Button from '.'
 
+const buttonExampleList = [
+  {
+    children: 'Default'
+  },
+  {
+    type: 'primary',
+    size: 'cta',
+    children: 'Primary'
+  },
+  {
+    type: 'success',
+    outline: true,
+    children: 'Success'
+  },
+  {
+    type: 'warning',
+    size: 'small',
+    children: 'Warning'
+  },
+  {
+    type: 'danger',
+    size: 'small',
+    outline: true,
+    children: 'Danger'
+  }
+]
+
 storiesOf('Button')
-  .addWithInfo('API', () => (
+  .addWithInfo('Default', `The default button.`, () => (
     <Button onClick={clickHandlerFixture}> 
       {stringFixture}
     </Button>
