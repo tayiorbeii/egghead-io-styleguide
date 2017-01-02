c72771abbc634b8a23c99c9864967943ba0eb531
Taylor Bell <taylorbell@gmail.com>
2017-01-02 13:03:30 -0700

    button examples

--- src/components/Button/index.examples.js
+++ src/components/Button/index.examples.js
@@ -30,17 +30,72 @@ const buttonExampleList = [
   }
 ]
 
-const buttonTypeExamples = ['default', 'primary', 'success', 'warning', 'danger']
+const standardButtonExamples = [
+  {type: 'default'},
+  {type: 'primary'},
+  {type: 'success'},
+  {type: 'warning'},
+  {type: 'danger'}
+]
 
-// Default Button
+// Standard Large Button (Default)
 storiesOf('Button')
-  .addWithInfo('Standard (Large)', `The Standard Button is available in several different flavors.`, () => (
+  .addWithInfo('Large (Default)', `The Large Default Button is available in several different flavors.`, () => (
     <div className='flex flex-row justify-center flex-wrap items-center'>
-      {buttonTypeExamples.map((btn, i) => {
+      {standardButtonExamples.map((btn, i) => {
         return (
           <div className='mb2 mh2' key={i}>
-            <Button type={btn} onClick={clickHandlerFixture}>
-              {btn}
+            <Button type={btn.type} onClick={clickHandlerFixture}>
+              {btn.type}
+            </Button>
+          </div>
+        )
+      })}
+    </div>
+  ))
+
+// Standard (Small) Button
+storiesOf('Button')
+  .addWithInfo('Small', `This Small Button is available in several different flavors.`, () => (
+    <div className='flex flex-row justify-center flex-wrap items-center'>
+      {standardButtonExamples.map((btn, i) => {
+        return (
+          <div className='mb2 mh2' key={i}>
+            <Button type={btn.type} size='small' onClick={clickHandlerFixture}>
+              {btn.type}
+            </Button>
+          </div>
+        )
+      })}
+    </div>
+  ))
+
+// Standard (CTA) Button
+storiesOf('Button')
+  .addWithInfo('CTA', `The CTA Button is sized for callouts.`, () => (
+    <div className='flex flex-row justify-center flex-wrap items-center'>
+      {standardButtonExamples.map((btn, i) => {
+        return (
+          <div className='mb2 mh2' key={i}>
+            <Button type={btn.type} size='cta' onClick={clickHandlerFixture}>
+              {btn.type}
+            </Button>
+          </div>
+        )
+      })}
+    </div>
+  ))
+
+
+// Outline Button
+storiesOf('Button')
+  .addWithInfo('Outline', `The Outline Button is available in all sizes (pictured in Large).`, () => (
+    <div className='flex flex-row justify-center flex-wrap items-center'>
+      {standardButtonExamples.map((btn, i) => {
+        return (
+          <div className='mb2 mh2' key={i}>
+            <Button type={btn.type} outline onClick={clickHandlerFixture}>
+              {btn.type}
             </Button>
           </div>
         )
