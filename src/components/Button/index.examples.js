3a5b7f786ba8f076b8cbd3de65c0d24ebd18c926
Taylor Bell <taylorbell@gmail.com>
2017-01-03 13:40:30 -0700

    cleanup buttons page

--- src/components/Button/index.examples.js
+++ src/components/Button/index.examples.js
@@ -4,107 +4,75 @@ import {stringFixture, clickHandlerFixture} from '../../utils/Fixtures'
 import Button from '.'
 
 const standardButtonExamples = [
-  {type: 'Primary'},
-  {type: 'Success'},
-  {type: 'Warning'},
-  {type: 'Danger'}
+  {type: 'default', children: 'Default'},
+  {type: 'primary', children: 'Primary'},
+  {type: 'success', children: 'Success'},
+  {type: 'warning', children: 'Warning'},
+  {type: 'danger', children: 'Danger'}
 ]
 
 
-const dispStyles = 'flex flex-column content-center justify-around flex-wrap items-center vh-100'
-const darkDecorator = `${dispStyles} bg-navy`
+const decoratorClasses = 'flex flex-column content-center justify-around flex-wrap items-center vh-100 bg-navy'
 const btnDisp = 'flex flex-column justify-around items-center vh-50'
 
 
 // Standard Large Button (Default)
 storiesOf('Button')
-  .addDecorator((story) => (<div className={darkDecorator}>{story()}</div>))
-
-  .addWithInfo('Large (Default)', `The Large Default Button is available in several different flavors.`, () => (
-    <Button onClick={clickHandlerFixture}>
-      Default
-    </Button>
-  ))
-
-storiesOf('Button')
-  .addDecorator((story) => (<div className={darkDecorator}>{story()}</div>))
-
-  .addWithInfo('Large (All Types)', `The Large Default Button is available in several different flavors.`, () => (
-    <div className={btnDisp}>
-      {standardButtonExamples.map((btn, i) => {
-        return (
-          <Button type={btn.type.toLowerCase()} onClick={clickHandlerFixture}>
-            {btn.type}
-          </Button>
-        )
-      })}
-    </div>
-  ))
+  .addDecorator((story) => (<div className={decoratorClasses}>{story()}</div>))
+  .addWithInfo('Large (Default)',
+    `The Large Default Button. Note: omitting 'type' will render the Default button.`,
+    () => (
+      <div className={btnDisp}>
+        {standardButtonExamples.map((btn, i) => {
+          return (
+            <Button type={btn.type} onClick={clickHandlerFixture}>
+              {btn.children}
+            </Button>
+          )
+        })}
+      </div>
+    )
+  )
 
 // Small Button
 storiesOf('Button')
-  .addDecorator((story) => (<div className={darkDecorator}>{story()}</div>))
-
-  .addWithInfo('Small (Default)', `The Small Button is available in several different flavors.`, () => (
-    <Button size='small' onClick={clickHandlerFixture}>
-      Default
-    </Button>
-  ))
-
-storiesOf('Button')
-  .addDecorator((story) => (<div className={darkDecorator}>{story()}</div>))
-
-  .addWithInfo('Small (All Types)', `The Small Button is available in several different flavors.`, () => (
-    <div className={btnDisp}>
-      {standardButtonExamples.map((btn, i) => {
-        return (
-          <Button type={btn.type.toLowerCase()} size='small' onClick={clickHandlerFixture}>
-            {btn.type}
-          </Button>
-        )
-      })}
-    </div>
-  ))
+  .addDecorator((story) => (<div className={decoratorClasses}>{story()}</div>))
+  .addWithInfo('Small',
+    `The Small Button. Note: omitting 'type' will render the Default button.`,
+    () => (
+      <div className={btnDisp}>
+        {standardButtonExamples.map((btn, i) => {
+          return (
+            <Button type={btn.type.toLowerCase()} size='small' onClick={clickHandlerFixture}>
+              {btn.type}
+            </Button>
+          )
+        })}
+      </div>
+    )
+  )
 
 // Extra Large Button
 storiesOf('Button')
-  .addDecorator((story) => (<div className={darkDecorator}>{story()}</div>))
-
-  .addWithInfo('Extra Large (Default)', `The Extra Large Button is available in several different flavors.`, () => (
-    <Button size='extra-large' onClick={clickHandlerFixture}>
-      Default
-    </Button>
-  ))
-
-storiesOf('Button')
-  .addDecorator((story) => (<div className={darkDecorator}>{story()}</div>))
-
-  .addWithInfo('Extra Large (All Types)', `The Extra Large Button is sized for callouts.`, () => (
-    <div className={btnDisp}>
-      {standardButtonExamples.map((btn, i) => {
-        return (
-          <Button type={btn.type.toLowerCase()} size='extra-large' onClick={clickHandlerFixture}>
-            {btn.type}
-          </Button>
-        )
-      })}
-    </div>
-  ))
+  .addDecorator((story) => (<div className={decoratorClasses}>{story()}</div>))
+  .addWithInfo('Extra Large', `The Extra Large Button is sized for callouts.`,
+    () => (
+      <div className={btnDisp}>
+        {standardButtonExamples.map((btn, i) => {
+          return (
+            <Button type={btn.type.toLowerCase()} size='extra-large' onClick={clickHandlerFixture}>
+              {btn.type}
+            </Button>
+          )
+        })}
+      </div>
+    )
+  )
 
 
 // Outline Button
 storiesOf('Button')
-  .addDecorator((story) => (<div className={dispStyles}>{story()}</div>))
-
-  .addWithInfo('Outline (Default)', `The Outline Button is available in all sizes (pictured in Large).`, () => (
-    <Button type='' onClick={clickHandlerFixture}>
-      Default
-    </Button>
-  ))
-
-storiesOf('Button')
-  .addDecorator((story) => (<div className={darkDecorator}>{story()}</div>))
-
+  .addDecorator((story) => (<div className={decoratorClasses}>{story()}</div>))
   .addWithInfo('Outline (All Types)', `The Outline Button is available in all sizes (pictured in Large).`, () => (
     <div className={btnDisp}>
       {standardButtonExamples.map((btn, i) => {
