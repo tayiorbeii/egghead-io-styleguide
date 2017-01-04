ab01aa370d0b67f537a04c37f9de348beccac95b
Nagalskiy Evgeniy <evgeniy.nagalskiy@gmail.com>
2017-01-04 23:07:39 +0200

    Remove unnecessary lines

--- src/components/Card/index.examples.js
+++ src/components/Card/index.examples.js
@@ -2,14 +2,16 @@ import React from 'react'
 import {storiesOf} from '@kadira/storybook'
 import Card from '.'
 
+const representClasses = 'flex justify-center items-center bg-dark-navy pv5'
+
 storiesOf('Static: Cards')
   .addWithInfo('Course Card', () => (
-    <div className='flex justify-center items-center bg-dark-navy pv5'>
+    <div className={representClasses}>
       <Card/>
     </div>
   ))
   .addWithInfo('Lesson Card', () => (
-    <div className='flex justify-center items-center bg-dark-navy pv5'>
+    <div className={representClasses}>
       <Card/>
     </div>
   ))
