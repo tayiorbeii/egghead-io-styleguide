3efac115449a005e22818597ea581ef3c12de342
Nagalskiy Evgeniy <evgeniy.nagalskiy@gmail.com>
2017-01-04 22:33:08 +0200

    Remove unnecessary lines

--- src/components/Card/index.examples.js
+++ src/components/Card/index.examples.js
@@ -4,16 +4,12 @@ import Card from '.'
 
 storiesOf('Static: Cards')
   .addWithInfo('Course Card', () => (
-    <div>
-      <div className='flex justify-center items-center bg-dark-navy pv5'>
-        <Card/>
-      </div>
+    <div className='flex justify-center items-center bg-dark-navy pv5'>
+      <Card/>
     </div>
   ))
   .addWithInfo('Lesson Card', () => (
-    <div>
-      <div className='flex justify-center items-center bg-dark-navy pv5'>
-        <Card/>
-      </div>
+    <div className='flex justify-center items-center bg-dark-navy pv5'>
+      <Card/>
     </div>
   ))
