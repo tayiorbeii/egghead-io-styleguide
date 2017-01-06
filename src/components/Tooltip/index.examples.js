158b80ea78cc474ce311f0d3ee4c148f0c925dc1
Taylor Bell <taylorbell@gmail.com>
2017-01-06 10:04:04 -0700

    fix tooltip examples

--- src/components/Tooltip/index.examples.js
+++ src/components/Tooltip/index.examples.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import {storiesOf} from '@kadira/storybook'
 import {nameFixture, avatarImageUrlFixture} from '../../utils/Fixtures'
-import Avatar from '.'
+import Tooltip, { PasswordTooltip } from '.'
 
-storiesOf('Avatar')
-  .addWithInfo('API', () => (
-    <Avatar
-      name={nameFixture}
-      url={avatarImageUrlFixture}
-    />
+const decoratorClasses = 'flex flex-column content-center justify-around flex-wrap items-center vh-100 bg-navy'
+
+storiesOf('Tooltip')
+  .addDecorator((story) => (<div className={decoratorClasses}>{story()}</div>))
+  .addWithInfo('Tooltip', () => (
+    <Tooltip>
+      Cool Tooltip!
+    </Tooltip>
+  ))
+storiesOf('Tooltip')
+  .addDecorator((story) => (<div className={decoratorClasses}>{story()}</div>))
+  .addWithInfo('Password Tooltip', () => (
+    <PasswordTooltip />
   ))
